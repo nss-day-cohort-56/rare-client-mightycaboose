8dd325e8899bc4e60d91fb478ddaa7c014e19e27
Charlie Murphey <murphey37@gmail.com>
2022-07-27 14:46:09 -0500

    committing before pull request for CG

---
+++ src/components/posts/postList.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+//import { DeleteButton } from "./DeleteButton"
+
+
+export const PostList = () => {
+    const [posts, setPosts] = useState([])
+    const navigate = useNavigate()
+    const [filteredPosts, setFiltered] = useState([])
+
+
+    const getPosts = () => {
+        fetch(`http://localhost:8088/posts`)
+        .then(response => response.json())
+        .then((postArray) => {
+            setPosts(postArray)
+                        })
+
+    }
+    useEffect(
+        () => {
+            getPosts()
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
+  
+    return <>
+       
+    <h2>Your Posts</h2>
+
+    
+
+    <article className="posts">
+        {
+            posts.map(
+                (post) => {
+                    return <section key={post.id} className="post">
+                        <Link to={`/posts/${post.id}/edit`}> {post.label}</Link>
+                        <div>{post.label}</div>
+                        <img src={post.image_url} alt={post.label}></img>
+                        <footer>
+                        <DeleteButton post={post} get={getPosts}/> 
+                    </footer>
+                    </section>
+                }
+            )
+        }
+
+
+    </article>
+    </>
+}
