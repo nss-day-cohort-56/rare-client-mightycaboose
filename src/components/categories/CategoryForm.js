60dad2d1eba2890d46bd186f33d12e05b9f0d0c9
Piper Leigh <pipergillianleigh@gmail.com>
2022-07-28 11:48:22 -0500

    Fixed sloppy joe code

--- src/components/categories/CategoryForm.js
+++ src/components/categories/CategoryForm.js
@@ -1,29 +1,21 @@
-import React, { useState, useEffect } from "react"
+import React, { useState} from "react"
 import { useNavigate } from "react-router-dom"
 import { addCategory } from "../../managers/CategoriesManager"
 
+//Defining a function to create form for new category
 export const CategoryForm = () => {
     const [updatedCategory, setUpdatedCategories] = useState({
         label: ""})
     const navigate = useNavigate()
 
-
+//Updates state when user types input
     const handleUpdateCategory = (event) => {
         const copy = {...updatedCategory} 
         copy.label = event.target.value 
         setUpdatedCategories(copy)
     }
 
-
-const constructNewCategory = () => {
-    // const copyCategory = { ...category }
-    // copyCategory.label = parseInt(copyCategory.label)
-    // if (!copyCategory.label) {
-    //     copyCategory.label = Date(Date.now()).toLocaleString('en-us').split('GMT')[0]
-    // }
-    // onFormSubmit(copyCategory)
-}
-
+//Button posts new object to category in database then navigates to category list
 return <>
     <input type="text" name="category" required value={updatedCategory.label} onChange={handleUpdateCategory} />
     <button className="submitNewCategoryB" type="submit"
@@ -31,18 +23,6 @@ return <>
 </>
 }
 
-
-/*
--Create input form
--When text entered into input form > text should be saved to state and value of input updated
-    -OnChange - make copy of current state
-    -Update value of copy with value from input (event)
-    -Save copy to state
-    -Value of input should = state
-
--Create save button
--When save button is clicked > value in state to be sent to database
-*/
 
 
 
